e88b01fecce6b8c3811d46adbd0e212994c325e8
Anna Harutyunyan <anna_h@zealous.tech>
2024-06-12 19:54:45 +0400

    Adedd comments

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+/**
+ * Print 1..100 numbers by using recursion
+ * @param {*} n 
+ */
 function printNums(n) {
   console.log(n + " number"), n < 100 ? (printNums(++n)) : null
 }
--- test.sql
+++ test.sql
@@ -1,7 +1,8 @@
--- S
+-- Will show the names of those who send a report to whom
+-- Table_name: users
+
+--Columns: user_id, name, reporter_id
 
 SELECT u1.name AS reporter_name, u2.name AS receiver_name
   FROM users u1
   JOIN users u2 ON u2.user_id = u1.reporter_id;
-
-
